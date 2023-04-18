e22ca152418ef81e27063fb73abf1db56257dc81
Ocel0tSec <78559938+Ocel0tSec@users.noreply.github.com>
2023-04-18 16:24:37 +0000

    cd into main directory

--- RunAzureHound.ps1
+++ RunAzureHound.ps1
@@ -1,1 +1,2 @@
 C:\Users\$([Environment]::UserName)\Desktop\AzureTools\AzureHound\.azurehound.exe start -j $auth list -o azure_out.json
+cd "C:\Users\$([Environment]::UserName)\Desktop\AzureDump-main"
