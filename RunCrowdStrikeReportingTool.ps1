661ee43578165d9b45b6e1f8f0ca69db39c64bc7
Ocel0tSec <78559938+Ocel0tSec@users.noreply.github.com>
2023-04-18 16:20:32 +0000

    Change back to main directory

--- RunCrowdStrikeReportingTool.ps1
+++ RunCrowdStrikeReportingTool.ps1
@@ -1,3 +1,4 @@
 #Run CRT (Give it a client code eventually)
 cd "C:\Users\$([Environment]::UserName)\Desktop\AzureTools\CRT" 
 .\Get-CRTReport.ps1 -JobName ClientName -WorkingDirectory "C:\Users\$([Environment]::UserName)\Desktop\AzFiles"
+cd "C:\Users\$([Environment]::UserName)\Desktop\AzureDump-main"
