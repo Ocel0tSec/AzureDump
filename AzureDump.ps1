82f6a0a374806fefde2adcdeb3d826931edcdc64
Ocel0tSec <78559938+Ocel0tSec@users.noreply.github.com>
2023-04-18 15:58:27 +0000

    Error fix

--- AzureDump.ps1
+++ AzureDump.ps1
@@ -1,9 +1,9 @@
-$configJsonPath = ".\config.json"
+$configJsonPath = "config.json"
 $configJson = Get-Content -Path $configJsonPath -Raw | ConvertFrom-Json
 
 foreach ($script in $configJson.scripts) {
     if ($script.enabled) {
-        Write-Host "Executing $($script.name) from $($script.file)" -ForegroundColor Cyan
-        .\$($script.file)
+        Write-Host "Executing $($script.name) from $($script.file)"
+        & ".\$($script.file)"
     }
 }
