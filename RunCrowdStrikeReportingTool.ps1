1b8a38f5120246b20666aebc7b8a310cb4b03039
Ocel0tSec <78559938+Ocel0tSec@users.noreply.github.com>
2023-04-18 13:20:19 +0000

    Initial Commit

---
+++ RunCrowdStrikeReportingTool.ps1
@@ -0,0 +1,3 @@
+#Run CRT (Give it a client code eventually)
+cd "C:\Users\$([Environment]::UserName)\Desktop\AzureTools\CRT" 
+.\Get-CRTReport.ps1 -JobName ClientName -WorkingDirectory "C:\Users\$([Environment]::UserName)\Desktop\AzFiles"
